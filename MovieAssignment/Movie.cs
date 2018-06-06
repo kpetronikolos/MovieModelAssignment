28ba27146703f1bb25c2225304d32fcc0544bfbf
konstantinos <kpetronikolos@gmail.com>
2018-06-06 04:16:31 +0300

    Add Movie class.

---
+++ MovieAssignment/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieAssignment
+{
+    class Movie
+    {
+        private Dictionary<int, string> _review;
+
+        public string Title { get; set; }
+        public double Rating { get; set; }
+        public double Money { get; set; }
+        public string Actor { get; set; }
+        public Dictionary<int, string> Review { get { return _review; } set { this._review = value; } }
+    }
+}
